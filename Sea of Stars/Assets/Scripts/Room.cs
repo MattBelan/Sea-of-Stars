cee38886be54d1b8137852f251f2e4a5d148f56f
MattBelan <mrb3617@rit.edu>
2019-10-28 19:14:33 -0400

    New Idea Prototype

---
+++ Sea of Stars/Assets/Scripts/Room.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public TestPlayerScript player;
+    public string roomName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            player.transform.position = transform.position;
+            player.currRoom = roomName;
+        }
+    }
+}
---
+++ Sea of Stars/Assets/Scripts/TestPlayerScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerScript : MonoBehaviour
+{
+    public GameObject foodBar;
+    public GameObject energyBar;
+    public GameObject directionBar;
+
+    float energy;
+    float food;
+    float direction;
+
+    public string currRoom;
+
+    public float gatherInterval;
+    float lastGatherTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        energy = 0;
+        food = 0;
+        direction = 0;
+        lastGatherTime = 0;
+        currRoom = "cabin";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastGatherTime > gatherInterval)
+        {
+            lastGatherTime = Time.time;
+
+            switch (currRoom)
+            {
+                case "engine":
+                    energy += 4;
+                    break;
+
+                case "kitchen":
+                    food += 4;
+                    break;
+
+                case "cabin":
+                    direction += 4;
+                    break;
+
+                default:
+                    break;
+            }
+
+            energy -= 1;
+            food -= 1;
+            direction -= 1;
+
+            if (energy < 0)
+            {
+                energy = 0;
+            }
+            if (food < 0)
+            {
+                food = 0;
+            }
+            if(direction < 0)
+            {
+                direction = 0;
+            }
+        }
+
+        Vector3 foodScale = foodBar.transform.localScale;
+        foodScale.x = food / 100;
+        foodBar.transform.localScale = foodScale;
+
+        Vector3 energyScale = energyBar.transform.localScale;
+        energyScale.x = energy / 100;
+        energyBar.transform.localScale = energyScale;
+
+        Vector3 directionScale = directionBar.transform.localScale;
+        directionScale.x = direction / 100;
+        directionBar.transform.localScale = directionScale;
+
+    }
+}
