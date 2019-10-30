f8d89c83ad579780bc20d2af7cb44c6da22147e5
Timothy Carter (RIT Student) <txc1321@ad.rit.edu>
2019-10-30 15:27:04 -0400

    New scene for square-breathing

---
+++ Sea of Stars/Assets/Scripts/SquareMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareMovement : MonoBehaviour
+{
+    public GameObject movingSquare;
+    public GameObject smallSquare;
+    public GameObject right;
+    public GameObject bottom;
+    public GameObject left;
+    public GameObject top;
+    public Sprite successSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
