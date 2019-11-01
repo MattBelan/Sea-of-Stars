6448956e317e97e4ca81f689b6672fc719eaa098
Jordan Machalek <jdm4344@rit.edu>
2019-10-31 22:59:11 -0400

    Started new version of crew functionality

---
+++ Sea of Stars/Assets/Scripts/CrewManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Manages interaction with crewmembers
+ */
+public class CrewManager : MonoBehaviour
+{
+    // Attributes
+    public List<CrewMember> crew;
+    public List<CrewMember> specialistCrew;
+    public int crewTotal;
+
+    private List<string> crewNames = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }; // Used to pick names when creating crew, uses List<> to remove names when used
+    private string[] crewRoles = { "Navigator", "Cook", "Engineer", "Gunner", "Loader"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Adds a sailor to the crew
+    public void RecruitCrew(bool isSpecialist = false)
+    {
+        // Get an index for the crew member's name
+        int nameNum = Random.Range(0, crewNames.Count - 1);
+        int roleNum = Random.Range(0, 4);
+
+        if (isSpecialist)
+        {
+            specialistCrew.Add(new CrewMember(crewNames[nameNum], crewRoles[roleNum], true));
+            crewNames.RemoveAt(nameNum);
+
+            return;
+        }
+
+        // Non-specialist crew member
+        crew.Add(new CrewMember(crewNames[nameNum], crewRoles[roleNum], false));
+        crewNames.RemoveAt(nameNum);
+    }
+}
---
+++ Sea of Stars/Assets/Scripts/CrewMember.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Represents a crew member on the ship
+ */
+public class CrewMember
+{
+    // Attributes
+    public string crewName;
+    public string role;
+    public bool isSpecialist = false;
+    
+    // Constructor
+    public CrewMember(string nm, string rl, bool sp)
+    {
+        crewName = nm;
+        role = rl;
+        isSpecialist = sp;
+    }
+
+
+}
