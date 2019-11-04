8d7743f167c52f075606483ba5979097dd4fed3c
MattBelan <mrb3617@rit.edu>
2019-11-03 21:13:42 -0500

    Combat Basics Implemented
    Click on the "squid" to attack it
    Squid attacks on a timer
    Console log is where combat stats and updates are displayed

---
+++ Sea of Stars/Assets/Scripts/CombatShip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatShip : CombatEntity
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        Health = 20;
+        Damage = 2;
+        FireRate = 5;
+        AttackReady = true;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public void Repair(float val)
+    {
+        Health += val;
+    }
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Repair(1);
+        }
+    }
+}
