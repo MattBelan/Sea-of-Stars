7d80f46e8748221a56591f0d7f5d406d65930dd4
Jordan Machalek <jdm4344@rit.edu>
2019-11-12 00:37:21 -0500

    Code Refactoring, Work on Specialist Functions Moved UIManager script to GameManager obj Added CrewManager to GameManager obj Added Text Objs for dialogue associated with rooms

--- Sea of Stars/Assets/Scripts/GameManager.cs
+++ Sea of Stars/Assets/Scripts/GameManager.cs
@@ -12,18 +12,6 @@ public class GameManager : MonoBehaviour
     public int fuelCount = 10; // This is separate to prevent the header from appearing above all 3 attributes
     public int foodCount = 10, luminosityCount = 10, minLuminosity = 0;
 
-    [Header("Crew Data")]
-    public int crewCount = 0;
-
-    [Header("UI References")]
-    public GameObject fuelTextObj;
-    public GameObject foodTextObj;
-    public GameObject crewTextObj;
-    public GameObject luminosityTextObj;
-    public GameObject eventTextObj;
-
-    [Header("Player Reference")]
-    public Ship player;
 
     private int foodCost;
 
@@ -36,34 +24,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Update UI every 30 frames
-        if(Time.frameCount % 30 == 0)
-        {
-            // TODO: Create additional variables and in Start() save Text component so they aren't being retrieved every time
-            // Also, maybe just use GameObject.Find() in Start - may have impact on initial loading time though
-            fuelTextObj.GetComponent<Text>().text = "Fuel: " + fuelCount;
-            foodTextObj.GetComponent<Text>().text = "Food: " + foodCount;
-            crewTextObj.GetComponent<Text>().text = "Crew " + crewCount;
-            luminosityTextObj.GetComponent<Text>().text = "Luminosity: " + luminosityCount;
-        }
-
-        foodCost = 1 + crewCount;
-
-        if (Time.frameCount % 480 == 0)
-        {
-            foodCount = foodCount - foodCost;
-            Debug.Log("Food decrease");
-            if (player.moving)
-            {
-                fuelCount--;
-                Debug.Log("Fuel decrease");
-            }
-        }
-    }
-
-    // Updates the UI in response to an event
-    public void AnnounceEvent(string message)
-    {
-        eventTextObj.GetComponent<Text>().text = message;
+      
     }
 }
